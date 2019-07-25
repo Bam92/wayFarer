d94db068bc374fa46d7623a92ad789efce6bc1c5
abelmbula <abelmbula@bam.com>
2019-07-25 21:53:43 +0200

    ft(server/tests):add unit test for trips

---
+++ server/tests/trip.spec.js
@@ -0,0 +1,29 @@
+import chaiHttp from 'chai-http';
+import chai from 'chai';
+import app from '../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const baseUrl = '/api/v1/auth/';
+
+describe ('/GET TRIPS', () => {
+  it('should retunr all trips', (done) => {
+    chai.request(app)
+      .get(`${baseUrl}trips`)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('status');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.have.property('trip_id');
+        expect(res.body.data).to.have.property('seating_capacity');
+        expect(res.body.data).to.have.property('origine');
+        expect(res.body.data).to.have.property('destination');
+        expect(res.body.data).to.have.property('trip_date');
+        expect(res.body.data).to.have.property('fare');
+        expect(res.body.data).to.have.property('status');
+        (done)
+      });
+  });
+});
