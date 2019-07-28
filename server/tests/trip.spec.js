755919e30c8e72a36200b50bbfb0ad0ebac7dd21
abelmbula <abelmbula@bam.com>
2019-07-28 21:40:27 +0200

    bg(trips test):fix some bugs that were preventing trips test to pass

--- server/tests/trip.spec.js
+++ server/tests/trip.spec.js
@@ -8,22 +8,23 @@ chai.use(chaiHttp);
 const baseUrl = '/api/v1/';
 
 describe ('/GET TRIPS', () => {
-  it('should return all trips', (done) => {
+  it('getTrips() should respond with 201 and return a list of all trips', (done) => {
     chai.request(app)
       .get(`${baseUrl}trips`)
       .end((err, res) => {
+        expect('Content-Type', /json/);
         expect(res).to.have.status(201);
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.property('status');
         expect(res.body).to.have.property('data');
-        expect(res.body.data).to.have.property('id');
-        expect(res.body.data).to.have.property('seating_capacity');
-        expect(res.body.data).to.have.property('origine');
-        expect(res.body.data).to.have.property('destination');
-        expect(res.body.data).to.have.property('trip_date');
-        expect(res.body.data).to.have.property('fare');
-        expect(res.body.data).to.have.property('status');
-        (done)
+        expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('seating_capacity');
+        expect(res.body.data[0]).to.have.property('origin');
+        expect(res.body.data[0]).to.have.property('destination');
+        expect(res.body.data[0]).to.have.property('trip_date');
+        expect(res.body.data[0]).to.have.property('fare');
+        expect(res.body.data[0]).to.have.property('status');
+        done();
       });
   });
 });
