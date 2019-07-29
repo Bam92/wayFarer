112b994f35e938c14aad1c77e9ca7fc40b6b8fb7
abelmbula <abelmbula@bam.com>
2019-07-29 17:19:56 +0200

    ft(api-tests):add unit tests for get trip/:id

--- server/tests/trip.spec.js
+++ server/tests/trip.spec.js
@@ -27,4 +27,37 @@ describe ('/GET TRIPS', () => {
         done();
       });
   });
+  it('getTrip() should respond with 201 and return detail of a given trip', (done) => {
+    chai.request(app)
+      .get(`${baseUrl}trips/1`)
+      .end((err, res) => {
+        //expect('Content-Type', /json/);
+        expect(res).to.have.status(201);
+        //expect(res).to.be.a('json');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('status');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array')
+          .that.includes.all.keys(['id', 'seating_capacity', 'origin', 'destination', 'trip_date', 'fare', 'status']);
+        /* expect(res.body.data[0]).to.have.property('id');
+        expect(res.body.data[0]).to.have.property('seating_capacity');
+        expect(res.body.data[0]).to.have.property('origin');
+        expect(res.body.data[0]).to.have.property('destination');
+        expect(res.body.data[0]).to.have.property('trip_date');
+        expect(res.body.data[0]).to.have.property('fare');
+        expect(res.body.data[0]).to.have.property('status');*/
+        done();
+      });
+  });
+  it('getTrip() should respond with 404 and return an error message if :id !== integer', (done) => {
+    chai.request(app)
+      .get(`${baseUrl}trips/nonExistingId`)
+      .end((err, res) => {
+        expect('Content-Type', /json/);
+        expect(res).to.have.status(404);
+        expect('Trip not found');
+        if (err) return done(err);
+        done();
+      });
+  });
 });
